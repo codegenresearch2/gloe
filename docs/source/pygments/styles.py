016614a49cb433b0ad64e0427e7f7f1fdf38c80b
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-08 00:58:32 +0000

    Reset to d7c5fc7f384d146e24953f6ea6b5353df4dca534 and made changes for LLM Round 1

--- docs/source/pygments/styles.py
+++ docs/source/pygments/styles.py
@@ -1,10 +1,12 @@
 from pygments.style import Style
-from pygments.token import (
-    Keyword, Name, Comment, String, Error, Number, Operator, Generic, Token, Whitespace
-)
+from pygments.token import (Keyword, Name, Comment, String, Error, Number, Operator, Generic, Token, Whitespace)
 
-class GloeStyle(Style):
+class GloeDarkStyle(Style):
     name = 'native'
+
+    background_color = '#202020'
+    highlight_color = '#404040'
+    line_number_color = '#aaaaaa'
 
     styles = {
         Token: '#d0d0d0',
@@ -13,16 +15,21 @@ class GloeStyle(Style):
         Comment.Preproc: 'noitalic bold #ff3a3a',
         Comment.Special: 'noitalic bold #e50808 bg:#520000',
         Keyword: 'bold #45df9a',
+        Keyword.Pseudo: 'nobold',
+        Operator.Word: 'bold #45df9a',
         String: '#6ad7ca',
+        String.Other: '#6ad7ca',
         Number: '#51b2fd',
         Name.Builtin: '#2fbccd',
         Name.Variable: '#40ffff',
+        Name.Constant: '#40ffff',
         Name.Class: 'underline #14c8ef',
         Name.Function: '#14c8ef',
         Name.Namespace: 'underline #14c8ef',
         Name.Exception: '#bbbbbb',
         Name.Tag: 'bold #45df9a',
         Name.Attribute: '#bbbbbb',
+        Name.Decorator: '#6ad7ca',
         Generic.Heading: 'bold #ffffff',
         Generic.Subheading: 'underline #ffffff',
         Generic.Deleted: '#ff3a3a',

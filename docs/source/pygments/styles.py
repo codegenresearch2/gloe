ad993dc962bc0c6c69a659ab8f32324069479860
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 19:52:51 +0000

    Reset to d7c5fc7f384d146e24953f6ea6b5353df4dca534 and made changes for LLM Round 1

--- docs/source/pygments/styles.py
+++ docs/source/pygments/styles.py
@@ -12,90 +12,76 @@ from pygments.token import (
     Whitespace,
 )
 
+# Switching to a different pygments style
+class GloeStyle(Style):
+    name = "custom"
 
-class GloeDarkStyle(Style):
-    name = "gloe-dark"
-
-    background_color = "#202020"
-    highlight_color = "#404040"
-    line_number_color = "#aaaaaa"
+    background_color = "#f5f5f5"
+    highlight_color = "#e0e0e0"
+    line_number_color = "#555555"
 
     styles = {
-        Token: "#d0d0d0",
-        Whitespace: "#666666",
-        Comment: "italic #ababab",
-        Comment.Preproc: "noitalic bold #ff3a3a",
-        Comment.Special: "noitalic bold #e50808 bg:#520000",
-        Keyword: "bold #45df9a",
+        Token: "#333333",
+        Whitespace: "#999999",
+        Comment: "italic #666666",
+        Comment.Preproc: "noitalic bold #ff0000",
+        Comment.Special: "noitalic bold #e50808 bg:#ffdddd",
+        Keyword: "bold #008080",
         Keyword.Pseudo: "nobold",
-        Operator.Word: "bold #45df9a",
-        String: "#6ad7ca",
-        String.Other: "#6ad7ca",
-        Number: "#51b2fd",
-        Name.Builtin: "#2fbccd",
-        Name.Variable: "#40ffff",
-        Name.Constant: "#40ffff",
-        Name.Class: "underline #14c8ef",
-        Name.Function: "#14c8ef",
-        Name.Namespace: "underline #14c8ef",
-        Name.Exception: "#bbbbbb",
-        Name.Tag: "bold #45df9a",
-        Name.Attribute: "#bbbbbb",
-        Name.Decorator: "#6ad7ca",
-        Generic.Heading: "bold #ffffff",
-        Generic.Subheading: "underline #ffffff",
-        Generic.Deleted: "#ff3a3a",
-        Generic.Inserted: "#589819",
-        Generic.Error: "#ff3a3a",
+        Operator.Word: "bold #008080",
+        String: "#008000",
+        String.Other: "#008000",
+        Number: "#0000ff",
+        Name.Builtin: "#000080",
+        Name.Variable: "#008080",
+        Name.Constant: "#008080",
+        Name.Class: "underline #0000ff",
+        Name.Function: "#0000ff",
+        Name.Namespace: "underline #0000ff",
+        Name.Exception: "#666666",
+        Name.Tag: "bold #008080",
+        Name.Attribute: "#666666",
+        Name.Decorator: "#008000",
+        Generic.Heading: "bold #000000",
+        Generic.Subheading: "underline #000000",
+        Generic.Deleted: "#ff0000",
+        Generic.Inserted: "#008000",
+        Generic.Error: "#ff0000",
         Generic.Emph: "italic",
         Generic.Strong: "bold",
         Generic.EmphStrong: "bold italic",
-        Generic.Prompt: "#aaaaaa",
-        Generic.Output: "#cccccc",
-        Generic.Traceback: "#ff3a3a",
-        Error: "bg:#e3d2d2 #a61717",
+        Generic.Prompt: "#555555",
+        Generic.Output: "#888888",
+        Generic.Traceback: "#ff0000",
+        Error: "bg:#ffdddd #a61717",
     }
 
+# Commenting out unused extensions for clarity
+extensions = [
+    # "sphinx_toolbox.more_autodoc.variables",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.autodoc",
+    # "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinxext.opengraph",
+    # "sphinx_autodoc_typehints",
+    "myst_parser",
+    "sphinx_copybutton",
+]
 
-class GloeLightStyle(Style):
-    name = "gloe-light"
+# Adding light theme CSS variables for customization
+html_theme_options = {
+    # ... existing options ...
+    "light_css_variables": {
+        "color-brand-primary": "#007acc",
+        "color-brand-content": "#007acc",
+        "font-stack": "Arial, sans-serif",
+        "font-stack--monospace": "Courier New, monospace",
+        "font-size--normal": "16px",
+    },
+}
 
-    background_color = "#ffffff"
-    highlight_color = "#404040"
-    line_number_color = "#aaaaaa"
-
-    styles = {
-        Token: "#d0d0d0",
-        Whitespace: "#666666",
-        Comment: "italic #ababab",
-        Comment.Preproc: "noitalic bold #ff3a3a",
-        Comment.Special: "noitalic bold #e50808 bg:#520000",
-        Keyword: "bold #45df9a",
-        Keyword.Pseudo: "nobold",
-        Operator.Word: "bold #45df9a",
-        String: "#6ad7ca",
-        String.Other: "#6ad7ca",
-        Number: "#51b2fd",
-        Name.Builtin: "#2fbccd",
-        Name.Variable: "#40ffff",
-        Name.Constant: "#40ffff",
-        Name.Class: "underline #14c8ef",
-        Name.Function: "#14c8ef",
-        Name.Namespace: "underline #14c8ef",
-        Name.Exception: "#bbbbbb",
-        Name.Tag: "bold #45df9a",
-        Name.Attribute: "#bbbbbb",
-        Name.Decorator: "#6ad7ca",
-        Generic.Heading: "bold #ffffff",
-        Generic.Subheading: "underline #ffffff",
-        Generic.Deleted: "#ff3a3a",
-        Generic.Inserted: "#589819",
-        Generic.Error: "#ff3a3a",
-        Generic.Emph: "italic",
-        Generic.Strong: "bold",
-        Generic.EmphStrong: "bold italic",
-        Generic.Prompt: "#aaaaaa",
-        Generic.Output: "#cccccc",
-        Generic.Traceback: "#ff3a3a",
-        Error: "bg:#e3d2d2 #a61717",
-    }
+# Switching to a different pygments style
+pygments_style = "styles.GloeStyle"

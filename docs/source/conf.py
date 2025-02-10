7becc370e5aeb37c420fdd224c6562109e58edf6
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 20:33:48 +0000

    Reset to d7c5fc7f384d146e24953f6ea6b5353df4dca534 and made changes for LLM Round 1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,8 +1,11 @@
 # Configuration file for the Sphinx documentation builder.
+# For more information about the configuration options, see the Sphinx documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 import os
 import sys
 
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 sys.path.insert(0, os.path.abspath("../.."))
 sys.path.insert(0, os.path.abspath("pygments"))
 
@@ -12,6 +15,8 @@ author = "Samir Braga"
 release = "0.4.3"
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
 extensions = [
     "sphinx_toolbox.more_autodoc.variables",
     "sphinx.ext.autosectionlabel",
@@ -21,6 +26,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.intersphinx",
     "sphinxext.opengraph",
+    # "sphinx_autodoc_typehints",
     "myst_parser",
     "sphinx_copybutton",
 ]
@@ -30,23 +36,49 @@ overloads_location = "bottom"
 napoleon_google_docstring = True
 autosectionlabel_prefix_document = True
 napoleon_use_rtype = False
-intersphinx_mapping = {"httpx": ("https://www.python-httpx.org/", None)}
+# intersphinx_mapping = {"httpx": ("https://www.python-httpx.org/", None)}
 ogp_site_url = "https://gloe.ideos.com.br/"
 ogp_image = "https://gloe.ideos.com.br/_static/assets/gloe-logo.png"
 
-# HTML output options
+# Templates and exclusions
+templates_path = ["_static"]
+exclude_patterns = ["Thumbs.db", ".DS_Store"]
+
+# Autodoc options
+autodoc_typehints = "description"
+autodoc_type_aliases = {
+    "PreviousTransformer": "gloe.base_transformer.PreviousTransformer"
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
 html_title = "Gloe"
+# html_logo = "assets/gloe-logo-small.png"
 html_theme = "furo"
 html_last_updated_fmt = ""
+# html_use_index = False  # Don't create index
+# html_domain_indices = False  # Don't need module indices
+# html_copy_source = False  # Don't need sources
 html_sidebars = {
     "Home": ["/"],
 }
+# autodoc_default_options = {"ignore-module-all": True}
+
 html_static_path = ["_static"]
 html_css_files = ["theme_customs.css"]
 html_favicon = "_static/assets/favicon.ico"
 html_theme_options = {
+    # "main_nav_links": {"Docs": "/index", "About": "/about"},
     "light_logo": "assets/gloe-logo-small.png",
     "dark_logo": "assets/gloe-logo-small.png",
+    "light_css_variables": {
+        "color-brand-primary": "#00e6bf",
+        "color-brand-content": "#00e6bf",
+        "font-stack": "Roboto, sans-serif",
+        "font-stack--monospace": "Courier, monospace",
+        "font-size--normal": "Courier, monospace",
+    },
     "dark_css_variables": {
         "color-brand-primary": "#00e6bf",
         "color-brand-content": "#00e6bf",
@@ -68,5 +100,6 @@ html_theme_options = {
     ],
 }
 
-# Pygments style options
+# Pygments styles
+pygments_light_style = "styles.GloeLightStyle"
 pygments_dark_style = "styles.GloeDarkStyle"

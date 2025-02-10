56cb9109d30b5c9885bdfda5f980d3696daa121d
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 22:27:38 +0000

    Reset to d7c5fc7f384d146e24953f6ea6b5353df4dca534 and made changes for LLM Round 1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,11 +30,6 @@ extensions = [
     "myst_parser",
     "sphinx_copybutton",
 ]
-overloads_location = "bottom"
-napoleon_google_docstring = True
-autosectionlabel_prefix_document = True
-napoleon_use_rtype = False
-# intersphinx_mapping = {"httpx": ("https://www.python-httpx.org/", None)}  # Commented out as per oracle feedback
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -42,13 +37,15 @@ napoleon_use_rtype = False
 html_title = "Gloe"
 html_theme = "furo"
 html_last_updated_fmt = ""
-# html_logo = "assets/gloe-logo-small.png"
-# html_use_index = False
-# html_domain_indices = False
-# html_copy_source = False
+html_logo = "assets/gloe-logo-small.png"
+html_use_index = False
+html_domain_indices = False
+html_copy_source = False
+
 html_sidebars = {
     "**": ["sidebar/brand.html", "sidebar/search.html", "sidebar/scroll-start.html", "sidebar/navigation.html", "sidebar/ethical-ads.html", "sidebar/scroll-end.html"]
 }
+
 templates_path = ["_templates"]
 exclude_patterns = ["Thumbs.db", ".DS_Store"]
 autodoc_typehints = "description"
@@ -56,41 +53,33 @@ autodoc_type_aliases = {
     "PreviousTransformer": "gloe.base_transformer.PreviousTransformer"
 }
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# Open Graph properties
+ogp_site_url = "https://gloe.ideos.com.br/"
+ogp_image = "https://gloe.ideos.com.br/_static/assets/gloe-logo.png"
 
-html_title = "Gloe"
-html_theme = "furo"
-html_last_updated_fmt = ""
-html_sidebars = {
-    "**": ["sidebar/brand.html", "sidebar/search.html", "sidebar/scroll-start.html", "sidebar/navigation.html", "sidebar/ethical-ads.html", "sidebar/scroll-end.html"]
+# Pygments styles
+pygments_light_style = "styles.GloeLightStyle"
+pygments_dark_style = "styles.GloeDarkStyle"
+
+# Light CSS variables
+light_css_variables = {
+    "color-brand-primary": "#00e6bf",
+    "color-brand-content": "#00e6bf",
+    "font-stack": "Roboto, sans-serif",
+    "font-stack--monospace": "Courier, monospace",
+    "font-size--normal": "Courier, monospace",
 }
-html_static_path = ["_static"]
-html_css_files = ["theme_customs.css"]
-html_favicon = "_static/assets/favicon.ico"
-html_theme_options = {
-    "light_logo": "assets/gloe-logo-small.png",
-    "dark_logo": "assets/gloe-logo-small.png",
-    "dark_css_variables": {
-        "color-brand-primary": "#00e6bf",
-        "color-brand-content": "#00e6bf",
-        "font-stack": "Roboto, sans-serif",
-        "font-stack--monospace": "Courier, monospace",
-        "font-size--normal": "Courier, monospace",
+
+# Footer icons
+footer_icons = [
+    {
+        "name": "GitHub",
+        "url": "https://github.com/ideos/gloe",
+        "html": """
+            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+            </svg>
+        """,
+        "class": "",
     },
-    "footer_icons": [
-        {
-            "name": "GitHub",
-            "url": "https://github.com/ideos/gloe",
-            "html": """
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
-                </svg>
-            """,
-            "class": "",
-        },
-    ],
-}
-
-# pygments_light_style = "styles.GloeLightStyle"  # Added as per oracle feedback
-pygments_dark_style = "styles.GloeDarkStyle"  # Added as per oracle feedback
+]

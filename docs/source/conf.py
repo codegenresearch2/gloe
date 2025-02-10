9bb1dad55ad41d4dc90d9b459b6155d557bb7bf7
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 21:30:06 +0000

    Reset to d7c5fc7f384d146e24953f6ea6b5353df4dca534 and made changes for LLM Round 1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,12 +22,11 @@ extensions = [
     "sphinx_toolbox.more_autodoc.variables",
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.autodoc",
-    # "sphinx.ext.autosummary",
+    "sphinx.ext.autosummary",
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
     "sphinx.ext.intersphinx",
     "sphinxext.opengraph",
-    # "sphinx_autodoc_typehints",
     "myst_parser",
     "sphinx_copybutton",
 ]
@@ -35,7 +34,7 @@ overloads_location = "bottom"
 napoleon_google_docstring = True
 autosectionlabel_prefix_document = True
 napoleon_use_rtype = False
-# intersphinx_mapping = {"httpx": ("https://www.python-httpx.org/", None)}
+intersphinx_mapping = {"httpx": ("https://www.python-httpx.org/", None)}
 ogp_site_url = "https://gloe.ideos.com.br/"
 ogp_image = "https://gloe.ideos.com.br/_static/assets/gloe-logo.png"
 
@@ -45,38 +44,25 @@ autodoc_typehints = "description"
 autodoc_type_aliases = {
     "PreviousTransformer": "gloe.base_transformer.PreviousTransformer"
 }
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_title = "Gloe"
-# html_logo = "assets/gloe-logo-small.png"
 html_theme = "furo"
 html_last_updated_fmt = ""
-# html_use_index = False  # Don't create index
-# html_domain_indices = False  # Don't need module indices
-# html_copy_source = False  # Don't need sources
-html_sidebars: dict[str, list[str]] = {
+html_sidebars = {
     "Home": ["/"],
 }
-# autodoc_default_options = {"ignore-module-all": True}
-
 html_static_path = ["_static"]
 html_css_files = ["theme_customs.css"]
 html_favicon = "_static/assets/favicon.ico"
 html_theme_options = {
-    # "main_nav_links": {"Docs": "/index", "About": "/about"},
     "light_logo": "assets/gloe-logo-small.png",
     "dark_logo": "assets/gloe-logo-small.png",
     "dark_css_variables": {
         "color-brand-primary": "#00e6bf",
         "color-brand-content": "#00e6bf",
-        "font-stack": "Roboto, sans-serif",
-        "font-stack--monospace": "Courier, monospace",
-        "font-size--normal": "Courier, monospace",
-    },
-    "light_css_variables": {
-        "color-brand-primary": "#00afac",
-        "color-brand-content": "#00afac",
         "font-stack": "Roboto, sans-serif",
         "font-stack--monospace": "Courier, monospace",
         "font-size--normal": "Courier, monospace",
@@ -95,6 +81,62 @@ html_theme_options = {
     ],
 }
 
-# pygments_style = "styles.GloeStyle"
-pygments_dark_style = "styles.GloeDarkStyle"
-pygments_light_style = "styles.GloeLightStyle"
+# Pygments styles
+from pygments.style import Style
+from pygments.token import (
+    Keyword,
+    Name,
+    Comment,
+    String,
+    Error,
+    Number,
+    Operator,
+    Generic,
+    Token,
+    Whitespace,
+)
+
+class GloeStyle(Style):
+    name = "gloe-style"
+
+    background_color = "#202020"
+    highlight_color = "#404040"
+    line_number_color = "#aaaaaa"
+
+    styles = {
+        Token: "#d0d0d0",
+        Whitespace: "#666666",
+        Comment: "italic #ababab",
+        Comment.Preproc: "noitalic bold #ff3a3a",
+        Comment.Special: "noitalic bold #e50808 bg:#520000",
+        Keyword: "bold #45df9a",
+        Keyword.Pseudo: "nobold",
+        Operator.Word: "bold #45df9a",
+        String: "#6ad7ca",
+        String.Other: "#6ad7ca",
+        Number: "#51b2fd",
+        Name.Builtin: "#2fbccd",
+        Name.Variable: "#40ffff",
+        Name.Constant: "#40ffff",
+        Name.Class: "underline #14c8ef",
+        Name.Function: "#14c8ef",
+        Name.Namespace: "underline #14c8ef",
+        Name.Exception: "#bbbbbb",
+        Name.Tag: "bold #45df9a",
+        Name.Attribute: "#bbbbbb",
+        Name.Decorator: "#6ad7ca",
+        Generic.Heading: "bold #ffffff",
+        Generic.Subheading: "underline #ffffff",
+        Generic.Deleted: "#ff3a3a",
+        Generic.Inserted: "#589819",
+        Generic.Error: "#ff3a3a",
+        Generic.Emph: "italic",
+        Generic.Strong: "bold",
+        Generic.EmphStrong: "bold italic",
+        Generic.Prompt: "#aaaaaa",
+        Generic.Output: "#cccccc",
+        Generic.Traceback: "#ff3a3a",
+        Error: "bg:#e3d2d2 #a61717",
+    }
+
+pygments_dark_style = "GloeStyle"

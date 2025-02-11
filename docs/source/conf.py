43eaecb5c8c82837c5c0f119f4ee960527a5b6e4
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 15:18:15 +0000

    Reset to d7c5fc7f384d146e24953f6ea6b5353df4dca534 and made changes for LLM Round 1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,6 @@ extensions = [
     "sphinx.ext.autosummary",
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
-    # "sphinx.ext.intersphinx",
     "sphinxext.opengraph",
     "myst_parser",
     "sphinx_copybutton",
@@ -38,7 +37,6 @@ overloads_location = "bottom"
 napoleon_google_docstring = True
 autosectionlabel_prefix_document = True
 napoleon_use_rtype = False
-# intersphinx_mapping = {"httpx": ("https://www.python-httpx.org/", None)}
 ogp_site_url = "https://gloe.ideos.com.br/"
 ogp_image = "https://gloe.ideos.com.br/_static/assets/gloe-logo.png"
 
@@ -62,19 +60,15 @@ autodoc_type_aliases = {
 html_title = "Gloe"
 html_theme = "furo"
 # html_last_updated_fmt = ""
-# html_use_index = False  # Don't create index
-# html_domain_indices = False  # Don't need module indices
-# html_copy_source = False  # Don't need sources
+# html_logo = "assets/gloe-logo-small.png"
 html_sidebars: dict[str, list[str]] = {
     "Home": ["/"],
 }
-# autodoc_default_options = {"ignore-module-all": True}
 
 html_static_path = ["_static"]
 html_css_files = ["theme_customs.css"]
 html_favicon = "_static/assets/favicon.ico"
 html_theme_options = {
-    # "main_nav_links": {"Docs": "/index", "About": "/about"},
     "light_logo": "assets/gloe-logo-small.png",
     "dark_logo": "assets/gloe-logo-small.png",
     "light_css_variables": {
@@ -150,3 +144,4 @@ class GloeLightStyle(Style):
     }
 
 pygments_style = "styles.GloeLightStyle"
+pygments_dark_style = "styles.GloeDarkStyle"

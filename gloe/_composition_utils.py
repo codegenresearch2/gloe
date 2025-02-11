4108016408f4bd3883051824fec29e24c3f25072
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-11 13:07:52 +0000

    Reset to d9e8fce3fd0a05435b82a5f09c0af03f6ff40505 and made changes for LLM Round 1

--- gloe/_composition_utils.py
+++ gloe/_composition_utils.py
@@ -4,24 +4,24 @@ from inspect import Signature
 from types import GenericAlias
 from typing import TypeVar, Any, cast
 
-from gloe import Transformer, AsyncTransformer, BaseTransformer
 from gloe._utils import _match_types, _specify_types
-from gloe.exceptions import UnsupportedTransformerArgException
 
 _In = TypeVar("_In")
 _Out = TypeVar("_Out")
 _NextOut = TypeVar("_NextOut")
 
 def is_transformer(node):
+    from gloe import Transformer
     return isinstance(node, Transformer) if not isinstance(node, (list, tuple)) else all(is_transformer(n) for n in node)
 
 def is_async_transformer(node):
+    from gloe import AsyncTransformer
     return isinstance(node, AsyncTransformer)
 
 def has_any_async_transformer(node: list):
     return any(is_async_transformer(n) for n in node)
 
-def _resolve_new_merge_transformers(new_transformer: BaseTransformer, transformer2: BaseTransformer):
+def _resolve_new_merge_transformers(new_transformer, transformer2):
     new_transformer.__class__.__name__ = transformer2.__class__.__name__
     new_transformer._label = transformer2.label
     new_transformer._children = transformer2.children
@@ -30,12 +30,13 @@ def _resolve_new_merge_transformers(new_transformer: BaseTransformer, transforme
     new_transformer._set_previous(transformer2.previous)
     return new_transformer
 
-def _resolve_serial_connection_signatures(transformer2: BaseTransformer, generic_vars: dict, signature2: Signature) -> Signature:
+def _resolve_serial_connection_signatures(transformer2, generic_vars, signature2):
     first_param = next(iter(signature2.parameters.values()))
     new_parameter = first_param.replace(annotation=_specify_types(transformer2.input_type, generic_vars))
     return signature2.replace(parameters=[new_parameter], return_annotation=_specify_types(signature2.return_annotation, generic_vars))
 
 def _merge_serial(transformer1, _transformer2):
+    from gloe import Transformer, AsyncTransformer, BaseTransformer, UnsupportedTransformerArgException
     transformer1 = transformer1.copy(regenerate_instance_id=True) if transformer1.previous is None else transformer1
     transformer2 = _transformer2.copy(regenerate_instance_id=True)
     transformer2._set_previous(transformer1)
@@ -72,6 +73,7 @@ def _merge_serial(transformer1, _transformer2):
     return _resolve_new_merge_transformers(NewTransformer(), transformer2)
 
 def _merge_diverging(incident_transformer, *receiving_transformers):
+    from gloe import Transformer, AsyncTransformer, BaseTransformer
     incident_transformer = incident_transformer.copy(regenerate_instance_id=True) if incident_transformer.previous is None else incident_transformer
     receiving_transformers = tuple(receiving_transformer.copy(regenerate_instance_id=True) for receiving_transformer in receiving_transformers)
 
@@ -106,7 +108,8 @@ def _merge_diverging(incident_transformer, *receiving_transformers):
 
     return new_transformer
 
-def _compose_nodes(current: BaseTransformer, next_node: tuple | BaseTransformer):
+def _compose_nodes(current, next_node):
+    from gloe import BaseTransformer, UnsupportedTransformerArgException
     if isinstance(current, BaseTransformer):
         if isinstance(next_node, BaseTransformer):
             return _merge_serial(current, next_node)

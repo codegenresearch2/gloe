212cfa71491beca22ecc5014af3d3970ca11deed
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-11 12:21:45 +0000

    Reset to d9e8fce3fd0a05435b82a5f09c0af03f6ff40505 and made changes for LLM Round 1

--- gloe/ensurer/_transformer_ensurer.py
+++ gloe/ensurer/_transformer_ensurer.py
@@ -1,4 +1,6 @@
-from typing import Any, Callable, Generic, ParamSpec, Sequence, TypeVar
+from abc import abstractmethod, ABC
+from typing import Any, Callable, Generic, ParamSpec, Sequence, TypeVar, cast
+from inspect import signature
 from gloe.async_transformer import AsyncTransformer
 from gloe.functional import _PartialTransformer, _PartialAsyncTransformer, Transformer
 
@@ -7,10 +9,12 @@ _S = TypeVar("_S")
 _U = TypeVar("_U")
 _P1 = ParamSpec("_P1")
 
-class TransformerEnsurer(Generic[_T, _S]):
+class TransformerEnsurer(Generic[_T, _S], ABC):
+    @abstractmethod
     def validate_input(self, data: _T):
         pass
 
+    @abstractmethod
     def validate_output(self, data: _T, output: _S):
         pass
 
@@ -25,17 +29,34 @@ class TransformerEnsurer(Generic[_T, _S]):
 
 def input_ensurer(func: Callable[[_T], Any]) -> TransformerEnsurer[_T, Any]:
     class LambdaEnsurer(TransformerEnsurer[_T, Any]):
+        __doc__ = func.__doc__
+        __annotations__ = cast(Callable, func).__annotations__
+
         def validate_input(self, data: _T):
             func(data)
     return LambdaEnsurer()
 
 def output_ensurer(func: Callable) -> TransformerEnsurer:
     class LambdaEnsurer(TransformerEnsurer):
+        __doc__ = func.__doc__
+        __annotations__ = cast(Callable, func).__annotations__
+
         def validate_output(self, data, output):
-            func(data, output) if len(func.__annotations__) > 1 else func(output)
+            if len(signature(func).parameters) == 1:
+                func(output)
+            else:
+                func(data, output)
     return LambdaEnsurer()
 
 class _ensure_base:
+    @abstractmethod
+    def _generate_new_transformer(self, transformer: Transformer) -> Transformer:
+        pass
+
+    @abstractmethod
+    def _generate_new_async_transformer(self, transformer: AsyncTransformer) -> AsyncTransformer:
+        pass
+
     def __call__(self, arg):
         if isinstance(arg, Transformer):
             return self._generate_new_transformer(arg)
@@ -46,16 +67,16 @@ class _ensure_base:
         if isinstance(arg, _PartialAsyncTransformer):
             return self._generate_new_partial_async_transformer(arg)
 
-    def _generate_new_transformer(self, transformer: Transformer) -> Transformer:
-        pass
-
-    def _generate_new_async_transformer(self, transformer: AsyncTransformer) -> AsyncTransformer:
-        pass
-
     def _generate_new_partial_transformer(self, transformer_init: _PartialTransformer) -> _PartialTransformer:
-        pass
+        def ensured_transformer_init(*args, **kwargs):
+            transformer = transformer_init(*args, **kwargs)
+            return self._generate_new_transformer(transformer)
+        return ensured_transformer_init
 
     def _generate_new_partial_async_transformer(self, async_transformer_init: _PartialAsyncTransformer) -> _PartialAsyncTransformer:
-        pass
+        def ensured_async_transformer_init(*args, **kwargs):
+            async_transformer = async_transformer_init(*args, **kwargs)
+            return self._generate_new_async_transformer(async_transformer)
+        return ensured_async_transformer_init
 
 # The rest of the code follows the same structure with similar modifications

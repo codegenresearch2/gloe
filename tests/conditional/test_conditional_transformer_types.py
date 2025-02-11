23022b04f376e2672532e25e86c95105e9668a1d
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 09:54:36 +0000

    Reset to 325a86b4021022d3a33a12237944a4647751c9a9 and made changes for LLM Round 1

--- tests/conditional/test_conditional_transformer_types.py
+++ tests/conditional/test_conditional_transformer_types.py
@@ -1,5 +1,6 @@
 import asyncio
 import unittest
+from typing import TypeVar, Union
 from typing_extensions import assert_type
 
 from gloe import (
@@ -16,11 +17,62 @@ from tests.lib.transformers import (
     minus1,
     to_string,
     tuple_concatenate,
+    logarithm,
+    repeat,
+    format_currency,
+    async_plus1,
 )
+from tests.lib.conditioners import if_not_zero, if_is_even
 from tests.type_utils.mypy_test_suite import MypyTestSuite
 
+In = TypeVar("In")
+Out = TypeVar("Out")
 
-class TestBasicTransformerTypes(MypyTestSuite):
+
+class TestTransformerTypes(MypyTestSuite):
+
+    def test_conditioned_flow_types(self):
+        """
+        Test the most simple transformer typing with conditioned flows
+        """
+        conditioned_graph = (
+            square >> square_root >> if_not_zero.Then(plus1).Else(minus1)
+        )
+        assert_type(conditioned_graph, Transformer[float, float])
+
+        conditioned_graph2 = (
+            square >> square_root >> if_not_zero.Then(to_string).Else(square)
+        )
+        assert_type(conditioned_graph2, Transformer[float, Union[str, float]])
+
+    def test_chained_condition_flow_types(self):
+        """
+        Test chained conditions in transformer flows
+        """
+        chained_conditions_graph = (
+            if_is_even.Then(square).ElseIf(lambda x: x < 10).Then(to_string).ElseNone()
+        )
+        assert_type(chained_conditions_graph, Transformer[float, Union[float, str, None]])
+
+    def test_async_chained_condition_flow_types(self):
+        """
+        Test async chained conditions in transformer flows
+        """
+        async_chained_conditions_graph = (
+            if_is_even.Then(async_plus1)
+            .ElseIf(lambda x: x < 10)
+            .Then(to_string)
+            .ElseNone()
+        )
+        assert_type(async_chained_conditions_graph, AsyncTransformer[float, Union[float, str, None]])
+
+        async_chained_conditions_graph = (
+            if_is_even.Then(square)
+            .ElseIf(lambda x: x < 10)
+            .Then(async_plus1)
+            .ElseNone()
+        )
+        assert_type(async_chained_conditions_graph, AsyncTransformer[float, Union[float, None]])
 
     def test_transformer_simple_typing(self):
         """

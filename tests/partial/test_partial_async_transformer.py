0b3c84d76aa637ba9c91d9bf9d91e471829c352a
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 09:21:44 +0000

    Reset to 325a86b4021022d3a33a12237944a4647751c9a9 and made changes for LLM Round 1

--- tests/partial/test_partial_async_transformer.py
+++ tests/partial/test_partial_async_transformer.py
@@ -1,12 +1,7 @@
 import asyncio
 import unittest
-
 from gloe import partial_async_transformer
 from gloe.utils import forward
-
-
-_DATA = {"foo": "bar"}
-
 
 class TestPartialAsyncTransformer(unittest.TestCase):
 

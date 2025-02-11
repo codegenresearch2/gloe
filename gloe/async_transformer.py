cf7aa58c014f1148d5f28a91d865d860efc8cdb7
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 09:31:20 +0000

    Reset to caa33e91923d85c059be44a856a031fea3f8e58d and made changes for LLM Round 1

--- gloe/async_transformer.py
+++ gloe/async_transformer.py
@@ -56,50 +56,34 @@ class AsyncTransformer(BaseTransformer[_In, _Out, "AsyncTransformer"], ABC):
         return f"{self.input_annotation} -> ({type(self).__name__}) -> {self.output_annotation}"
 
     async def __call__(self, data: _In) -> _Out:
-        transform_exception = None
-
-        transformed: _Out | None = None
         try:
-            transformed = await self.transform_async(data)
-        except Exception as exception:
-            if type(exception.__cause__) == TransformerException:
-                transform_exception = exception.__cause__
+            return await self.transform_async(data)
+        except Exception as e:
+            tb = traceback.extract_tb(e.__traceback__)
+            transformer_frames = [
+                frame
+                for frame in tb
+                if frame.name == self.__class__.__name__ or frame.name == "transform_async"
+            ]
+
+            if len(transformer_frames) == 1:
+                transformer_frame = transformer_frames[0]
+                exception_message = (
+                    f"\n  "
+                    f'File "{transformer_frame.filename}", line {transformer_frame.lineno}, '
+                    f'in transformer "{self.__class__.__name__}"\n  '
+                    f"  >> {transformer_frame.line}"
+                )
             else:
-                tb = traceback.extract_tb(exception.__traceback__)
-
-                # TODO: Make this filter condition stronger
-                transformer_frames = [
-                    frame
-                    for frame in tb
-                    if frame.name == self.__class__.__name__ or frame.name == "transform"
-                ]
-
-                if len(transformer_frames) == 1:
-                    transformer_frame = transformer_frames[0]
-                    exception_message = (
-                        f"\n  "
-                        f'File "{transformer_frame.filename}", line {transformer_frame.lineno}, '
-                        f'in transformer "{self.__class__.__name__}"\n  '
-                        f"  >> {transformer_frame.line}"
-                    )
-                else:
-                    exception_message = (
-                        f'An error occurred in transformer "{self.__class__.__name__}"'
-                    )
-
-                transform_exception = TransformerException(
-                    internal_exception=exception,
-                    raiser_transformer=self,
-                    message=exception_message,
+                exception_message = (
+                    f'An error occurred in transformer "{self.__class__.__name__}"'
                 )
 
-        if transform_exception is not None:
-            raise transform_exception.internal_exception
-
-        if type(transformed) is not None:
-            return cast(_Out, transformed)
-
-        raise NotImplementedError  # pragma: no cover
+            raise TransformerException(
+                internal_exception=e,
+                raiser_transformer=self,
+                message=exception_message,
+            )
 
     def copy(
         self,
@@ -116,7 +100,6 @@ class AsyncTransformer(BaseTransformer[_In, _Out, "AsyncTransformer"], ABC):
             copied.instance_id = uuid.uuid4()
 
         if self.previous is not None:
-            # copy_next_previous = 'none' if copy_previous == 'first_previous' else copy_previous
             if type(self.previous) == tuple:
                 new_previous: list[BaseTransformer] = [
                     previous_transformer.copy() for previous_transformer in self.previous
@@ -214,4 +197,31 @@ class AsyncTransformer(BaseTransformer[_In, _Out, "AsyncTransformer"], ABC):
         pass
 
     def __rshift__(self, next_node):
-        pass  # pragma: no cover
+        if isinstance(next_node, BaseTransformer):
+            return self._chain_transformers(next_node)
+        elif isinstance(next_node, tuple):
+            return self._chain_multiple_transformers(next_node)
+        else:
+            raise TypeError("next_node must be a BaseTransformer or a tuple of BaseTransformers")
+
+    def _chain_transformers(self, next_node: BaseTransformer[_Out, _NextOut, Any]) -> "AsyncTransformer[_In, _NextOut]":
+        async def chained_transform_async(data: _In) -> _NextOut:
+            intermediate_result = await self.transform_async(data)
+            return await next_node.transform(intermediate_result)
+
+        new_transformer = copy.copy(self)
+        new_transformer.transform_async = types.MethodType(chained_transform_async, new_transformer)
+        return new_transformer
+
+    def _chain_multiple_transformers(self, next_nodes: tuple[BaseTransformer[_Out, _NextOut, Any], ...]) -> "AsyncTransformer[_In, tuple[_NextOut, ...]]":
+        async def chained_transform_async(data: _In) -> tuple[_NextOut, ...]:
+            intermediate_result = await self.transform_async(data)
+            results = [intermediate_result]
+            for next_node in next_nodes:
+                result = await next_node.transform(results[-1])
+                results.append(result)
+            return tuple(results)
+
+        new_transformer = copy.copy(self)
+        new_transformer.transform_async = types.MethodType(chained_transform_async, new_transformer)
+        return new_transformer

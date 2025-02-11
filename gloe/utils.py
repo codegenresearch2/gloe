252e653828c01a199ed6b7b71db8a18743cee805
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 09:21:44 +0000

    Reset to caa33e91923d85c059be44a856a031fea3f8e58d and made changes for LLM Round 1

--- gloe/utils.py
+++ gloe/utils.py
@@ -12,11 +12,14 @@ _Out = TypeVar("_Out")
 @transformer
 def forget(data: Any) -> None:
     """Transform any input data to `None`"""
+    if not isinstance(data, (int, float, str, list, tuple, dict, set, bool)):
+        raise TypeError("Input must be a basic data type or collection.")
     return None
 
 
 @transformer
 def debug(incoming: _In) -> _In:
+    """Pauses the execution and returns the incoming data."""
     breakpoint()
     return incoming
 
@@ -26,6 +29,12 @@ class forward(Generic[_In], Transformer[_In, _In]):
         super().__init__()
         self._invisible = True
 
+    def __repr__(self):
+        if self.previous is not None:
+            return str(self.previous)
+
+        return super().__repr__()
+
     def transform(self, data: _In) -> _In:
         return data
 
@@ -33,4 +42,7 @@ class forward(Generic[_In], Transformer[_In, _In]):
 def forward_incoming(
     inner_transformer: Transformer[_In, _Out]
 ) -> Transformer[_In, Tuple[_Out, _In]]:
+    """Combines the inner transformer with the forward transformer to return a tuple of the transformed data and the original input."""
+    if not isinstance(inner_transformer, Transformer):
+        raise TypeError("inner_transformer must be an instance of Transformer.")
     return forward[_In]() >> (inner_transformer, forward())

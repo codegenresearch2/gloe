52203e5f7d35862da3258dce135b57d7634338e4
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-11 14:39:34 +0000

    Reset to 325a86b4021022d3a33a12237944a4647751c9a9 and made changes for LLM Round 1

--- tests/partial/test_partial_async_transformer.py
+++ tests/partial/test_partial_async_transformer.py
@@ -3,7 +3,9 @@ import unittest
 from gloe import partial_async_transformer
 from gloe.utils import forward
 
-class TestPartialAsyncTransformer(unittest.IsolatedAsyncioTestCase):
+_DATA = {"foo": "bar"}
+
+class TestPartialAsyncTransformer(unittest.TestCase):
 
     async def test_partial_async_transformer(self):
         @partial_async_transformer
@@ -13,9 +15,6 @@ class TestPartialAsyncTransformer(unittest.IsolatedAsyncioTestCase):
 
         pipeline = sleep_and_forward(0.01) >> forward()
 
-        result = await pipeline({"foo": "bar"})
+        result = await pipeline(_DATA)
 
-        self.assertDictEqual(result, {"foo": "bar"})
-
-
-In the rewritten code, I have removed the unused import for `unittest.TestCase` and replaced it with `unittest.IsolatedAsyncioTestCase` since we are using asynchronous tests. I have also updated the assertion to use `self.assertDictEqual` to compare dictionaries. Additionally, I have provided a dictionary to the `pipeline` function call as it was not defined in the original code.
+        self.assertEqual(result, _DATA)
